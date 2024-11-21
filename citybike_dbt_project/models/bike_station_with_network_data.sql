c0a1588c577bded3f571e08790d24f289ea7b1be
nikagos <nikagos@hotmail.com>
2024-11-21 00:44:31 +0000

    changed dbt model, added architecture image

--- citybike_dbt_project/models/bike_station_with_network_data.sql
+++ citybike_dbt_project/models/bike_station_with_network_data.sql
@@ -1,15 +1,19 @@
-SELECT bs.*
-     , n.city AS network_city
-     , c.company_id
-     , n.company
-     , n.network_name
-     , n.country AS network_country
-FROM 
-    {{ source('dezoomcamp', 'bike_station_data') }} bs
-JOIN 
-    {{ source('dezoomcamp', 'networks') }} n
-ON bs.network_id = n.network_id
-LEFT JOIN 
-    {{ ref('companies') }} c
-ON n.company = c.company
-AND n.country = c.country
+SELECT * EXCEPT (timestamp)
+FROM (
+        SELECT bs.* 
+            , FORMAT_TIMESTAMP('%Y-%m-%d %H:%M:%S', TIMESTAMP(bs.timestamp), 'America/New_York') AS timestamp_ny
+            , n.city AS network_city
+            , c.company_id
+            , n.company
+            , n.network_name
+            , n.country AS network_country
+        FROM 
+            {{ source('dezoomcamp', 'bike_station_data') }} bs
+        JOIN 
+            {{ source('dezoomcamp', 'networks') }} n
+        ON bs.network_id = n.network_id
+        LEFT JOIN 
+            {{ ref('companies') }} c
+        ON n.company = c.company
+        AND n.country = c.country
+)
